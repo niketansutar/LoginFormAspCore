b0abb0b96d908ea78e751e0caf6c0d12cf07f36d
Niketan <Niketan@LAPTOP-K0VQKJQP>
2024-12-09 12:21:14 +0530

    Add project files.

---
+++ LoginFormAspCore/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+using LoginFormAspCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace LoginFormAspCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CodeFirstDbContext context;
+        public HomeController(CodeFirstDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                return RedirectToAction("Dashboard");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserId user)
+        {
+            var myUser = context.UserIds.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault();
+            if (myUser != null)
+            {
+                HttpContext.Session.SetString("UserSession", myUser.Email);
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "Login Failed";
+            }
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        public IActionResult LoginOut()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+
+
+
+        // Signip / Registeres Form
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserId user)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.UserIds.AddAsync(user);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "Registered Successfully..";
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LoginFormAspCore/Models/CodeFirstDbContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginFormAspCore.Models;
+
+public partial class CodeFirstDbContext : DbContext
+{
+    public CodeFirstDbContext()
+    {
+    }
+
+    public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    public virtual DbSet<UserId> UserIds { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Designation)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserId>(entity =>
+        {
+            entity.ToTable("User_Id");
+
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
